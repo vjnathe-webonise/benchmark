96bd365d911cce62cf9fa6199b685fc663b2e52b
vijayraj <vijayraj.nathe@weboniselab.com>
2018-02-21 18:45:24 +0530

    added some basic test cases

---
+++ test.js
@@ -0,0 +1,42 @@
+var Benchmark = require('benchmark');
+
+var suite = new Benchmark.Suite;
+var assert = require('assert');
+// add tests 
+/*suite.add('RegExp#test', function() {
+  /o/.test('Hello World!');
+})
+.add('String#indexOf', function() {
+  'Hello World!'.indexOf('o') > -1;
+})
+// add listeners 
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async 
+.run({ 'async': true });*/
+
+convert = "1234"
+suite.add('parseInt', () => {
+    assert(parseInt(convert, 10) , 1234)
+})
+.add('Number(x)', () => {
+    assert(Number(convert) , 1234)
+})
+.add( '~~convert', () => {
+    assert((~~convert) , 1234)
+})
+.add('convert|0', () => {
+    assert((convert|0) , 1234)
+})
+.add('+convert', () => {
+    assert((+convert) , 1234)
+})
+.on('complete', (b) => { 
+   console.log(b);
+})
+.run({async : true}); 
+    
