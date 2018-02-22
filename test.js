b281b70b83d24ac3f41f2a22c64f24aaf4bce182
vijayraj <vijayraj.nathe@weboniselab.com>
2018-02-22 13:12:15 +0530

    added some lodash vs underscore vs native benchmark

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ var Benchmark = require('benchmark');
 
 var suite = new Benchmark.Suite;
 var assert = require('assert');
+var lodash = require('lodash');
+var underscore = require('underscore');
+
 // add tests 
 /*suite.add('RegExp#test', function() {
   /o/.test('Hello World!');
@@ -19,7 +22,7 @@ var assert = require('assert');
 // run async 
 .run({ 'async': true });*/
 
-convert = "1234"
+/*convert = "1234"
 suite.add('parseInt', () => {
     assert(parseInt(convert, 10) , 1234)
 })
@@ -40,5 +43,38 @@ suite.add('parseInt', () => {
     console.log('Fastest is ' + this.filter('fastest').map('name'));
     console.log('Slowest is ' + this.filter('slowest').map('name'));
 })
-.run({async : true}); 
+.run({async : true});*/ 
     
+var arr = [1,2,3,4,5,6,7,8,9,10];
+suite.add('lodash.each', () => {
+    lodash.each(arr, (a) => a);
+})
+.add('underscore.each', () => {
+    underscore.each(arr, (a) => a);
+})
+.add( 'forEach', () => {
+    arr.forEach((a) => a);
+})
+.on('complete', function()  { 
+    this.forEach((test) => console.log(`Test: ${test.name}, Operations: ${test.hz}`));
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+    console.log('Slowest is ' + this.filter('slowest').map('name'));
+})
+.run({async : true});
+
+var suite1 = new Benchmark.Suite;
+suite1.add('lodash.map', () => {
+    lodash.map(arr, (a) => a);
+})
+.add('underscore.map', () => {
+    underscore.map(arr, (a) => a);
+})
+.add( 'map', () => {
+    arr.map((a) => a);
+})
+.on('complete', function()  { 
+    this.forEach((test) => console.log(`Test: ${test.name}, Operations: ${test.hz}`));
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+    console.log('Slowest is ' + this.filter('slowest').map('name'));
+})
+.run({async : true});
